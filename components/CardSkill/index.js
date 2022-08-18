d9770ad09192639b37b8c787f6e5bcf648cb40fb
Carlos Cifuentes <cclaguia@gmail.com>
2022-08-18 15:05:02 +0200

    fix: noreferrer

--- components/CardSkill/index.js
+++ components/CardSkill/index.js
@@ -4,10 +4,10 @@ const CardSkill = (props) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardLogo}>
-        <a href={props.link} target="_blank">{props.logo}</a>
+        <a href={props.link} target="_blank" rel="noreferrer">{props.logo}</a>
       </div>
       <div className={styles.cardTitle}>
-        <a href={props.link} target="_blank">{props.title}</a>
+        <a href={props.link} target="_blank" rel="noreferrer">{props.title}</a>
       </div>
       <div className={styles.cardText}>
         {props.text}
